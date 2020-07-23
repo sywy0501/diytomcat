1d4380e9d1a7423972fda18c29b5dce62bd01176
chus <869187331@qq.com>
2020-07-23 16:24:29 +0800

    添加对cookie的处理

--- src/com/cs/tomcat/http/Response.java
+++ src/com/cs/tomcat/http/Response.java
@@ -1,14 +1,17 @@
 package com.cs.tomcat.http;
 
-import javax.servlet.ServletOutputStream;
+import cn.hutool.core.date.DateField;
+import cn.hutool.core.date.DateUtil;
+import cn.hutool.log.LogFactory;
+
 import javax.servlet.http.Cookie;
-import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
 import java.io.PrintWriter;
 import java.io.StringWriter;
-import java.io.UnsupportedEncodingException;
 import java.nio.charset.StandardCharsets;
-import java.util.Collection;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
 import java.util.Locale;
 
 /**
@@ -23,6 +26,7 @@ public class Response extends BaseResponse {
     private String contentType;
     private byte[] body;
     private int status;
+    private List<Cookie> cookies;
 
     @Override
     public void setStatus(int status){
@@ -50,17 +54,56 @@ public class Response extends BaseResponse {
         this.stringWriter = new StringWriter();
         this.contentType = "text/html";
         this.printWriter = new PrintWriter(stringWriter);
+        this.cookies = new ArrayList<>();
     }
 
+    @Override
     public PrintWriter getWriter(){
         return printWriter;
     }
 
+    @Override
     public void setContentType(String contentType){
         this.contentType = contentType;
     }
 
+    @Override
     public String getContentType(){
         return contentType;
+    }
+
+    @Override
+    public void addCookie(Cookie cookie){
+        cookies.add(cookie);
+    }
+
+    public List<Cookie> getCookies(){
+        return this.cookies;
+    }
+
+    public String getCookiesHeader(){
+        if (null==cookies){
+            return "";
+        }
+        String pattern = "EEE,d MMM yyyy HH:mm:ss 'GMT'";
+        SimpleDateFormat sdf = new SimpleDateFormat(pattern, Locale.ENGLISH);
+        StringBuilder sb = new StringBuilder();
+        for (Cookie cookie:getCookies()){
+            sb.append("\r\n");
+            sb.append("Set-Cookie:");
+            LogFactory.get().info(cookie.getName()+"="+cookie.getValue()+";");
+            sb.append(cookie.getName()+"="+cookie.getValue()+";");
+            if (-1!=cookie.getMaxAge()){
+                sb.append("Expires=");
+                Date now = new Date();
+                Date expire = DateUtil.offset(now, DateField.MINUTE,cookie.getMaxAge());
+                sb.append(sdf.format(expire));
+                sb.append(",");
+            }
+            if (null!=cookie.getPath()){
+                sb.append("Path="+cookie.getPath());
+            }
+        }
+        return sb.toString();
     }
 }
--- src/com/cs/tomcat/util/Constant.java
+++ src/com/cs/tomcat/util/Constant.java
@@ -11,7 +11,7 @@ import java.io.File;
  */
 public class Constant {
 
-    public final static String RESPONSE_HEAD_202 = "HTTP/1.1 200 OK\r\n" + "Content-Type:{}\r\n\r\n";
+    public final static String RESPONSE_HEAD_202 = "HTTP/1.1 200 OK\r\n" + "Content-Type:{}{}\r\n\r\n";
 
     public final static File webappsFolder = new File(SystemUtil.get("user.dir"), "webapps");
     public final static File rootFolder = new File(webappsFolder, "ROOT");
