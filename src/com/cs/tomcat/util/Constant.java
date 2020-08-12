806469efb1ebf408333616c999ca7a2db0eb2f1a
chus <869187331@qq.com>
2020-08-12 16:35:39 +0800

    jsp的转译和编译

--- src/com/cs/tomcat/util/Constant.java
+++ src/com/cs/tomcat/util/Constant.java
@@ -19,6 +19,8 @@ public class Constant {
     public final static File confFolder = new File(SystemUtil.get("user.dir"), "conf");
     public final static File serverXmlFile = new File(confFolder, "server.xml");
 
+    public static final String workFolder = SystemUtil.get("user.dir")+File.separator+"work";
+
     public static final String response_head_404 =
             "HTTP/1.1 404 Not Found\r\n" +
                     "Content-Type: text/html\r\n\r\n";
